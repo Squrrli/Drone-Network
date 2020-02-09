090f608b82c54e4c36584ecde37ceabecf61ecff
Adam <omahonyadam@gmail.com>
2020-02-09 14:57:14 +0000

    change to Behavior[Command], add message adapter to allow handling messages from both Recptionist and other actors

--- src/main/scala/ie/ul/dronenet/actors/DroneManager.scala
+++ src/main/scala/ie/ul/dronenet/actors/DroneManager.scala
@@ -6,22 +6,25 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.{Actor, ActorLogging, Props}
 
 object DroneManager {
-  def apply(managerId: String, localDrone: ActorRef[Drone.Command]): Behavior[Receptionist.Listing] = {
-    Behaviors
-      .setup[Receptionist.Listing] { context =>
-        context.log.debug("Drone Manager starting")
 
-        context.system.receptionist ! Receptionist.Subscribe(Drone.DroneServiceKey, context.self)
+  sealed trait Command
+  private case class ListingResponse(listing: Receptionist.Listing) extends Command
+
+  def apply(managerId: String, localDrone: ActorRef[Drone.Command]): Behavior[Command] = {
+    Behaviors.setup[Command] { context =>
+        val messageAdapter = context.messageAdapter[Receptionist.Listing](ListingResponse)
+
+        context.system.receptionist ! Receptionist.Subscribe(Drone.DroneServiceKey, messageAdapter)
 //        context.system.receptionist ! Receptionist.Subscribe(BaseManager.BaseManagerServiceKey, context.self)
 
-        Behaviors.receiveMessagePartial[Receptionist.Listing] {
-          case Drone.DroneServiceKey.Listing(listings) =>
+        Behaviors.receiveMessagePartial[Command] {
+          case ListingResponse(Drone.DroneServiceKey.Listing(listings)) =>
             listings.foreach(d => context.log.info("Drone Path received from Receptionist Listing: {}", d.path))
             Behaviors.same
 //          case BaseManager.BaseManagerServiceKey.Listing(listings) =>
 //            listings.foreach(bm => context.log.info("BaseManager Path received from Receptionist Listing: {}", bm.path))
 //            Behaviors.same
         }
-      }
+    }
   }
 }
