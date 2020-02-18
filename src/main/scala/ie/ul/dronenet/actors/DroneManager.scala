d779011f30619f3e2157a374d9fb6fc135f7920b
Adam <omahonyadam@gmail.com>
2020-02-18 16:17:06 +0000

    change to have DroneManager parent of Drone

--- src/main/scala/ie/ul/dronenet/actors/DroneManager.scala
+++ src/main/scala/ie/ul/dronenet/actors/DroneManager.scala
@@ -1,35 +1,72 @@
 package ie.ul.dronenet.actors
 
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.{ActorRef, Behavior, receptionist}
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.{Actor, ActorLogging, Props}
-import ie.ul.dronenet.actors.Drone.DroneServiceKey
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, GroupRouter, Routers}
+import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
+import ie.ul.dronenet.actors.CborSerializable
+
+import scala.collection.mutable
 
 object DroneManager {
-
   val DroneManagerServiceKey: ServiceKey[Command] = ServiceKey[Command]("droneManagerService")
 
-  sealed trait Command
+  val routerGroup: GroupRouter[BaseManager.Command] = Routers.group(BaseManager.BSManagerServiceKey)
+
+  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+  @JsonSubTypes(
+    Array(new JsonSubTypes.Type(value = classOf[RequestBaseStation]))
+  )
+  sealed trait Command extends CborSerializable
+
+  final case object DroneReady extends Command
   private case class ListingResponse(listing: Receptionist.Listing) extends Command
 
-  def apply(managerId: String, localDrone: ActorRef[Drone.Command]): Behavior[Command] = {
+  final case class RequestBaseStation(reqId: Long, drone: ActorRef[Drone.Command]) extends Command
+
+  def apply(managerId: String): Behavior[Command] = {
     Behaviors.setup[Command] { context =>
-        val messageAdapter = context.messageAdapter[Receptionist.Listing](ListingResponse)
+        val drone = context.spawn(Drone(managerId, context.self), "drone-" + managerId)
+        val msgAdapterListingResponse = context.messageAdapter[Receptionist.Listing](ListingResponse)
+        val routerGroup: GroupRouter[BaseManager.Command] = Routers.group(BaseManager.BSManagerServiceKey)
+        val router = context.spawn(routerGroup.withRoundRobinRouting(), "baseStationManager-group")
 
+        var managerNotReady: Boolean = true
+
+        // Register with local Receptionist and Subscribe to Listings of relevant ServiceKeys
         context.system.receptionist ! Receptionist.register(DroneManagerServiceKey, context.self)
-      // Subscribe to relevant Receptionist Listings
-        context.system.receptionist ! Receptionist.Subscribe(Drone.DroneServiceKey, messageAdapter)
-        context.system.receptionist ! Receptionist.Subscribe(BaseManager.BSManagerServiceKey, messageAdapter)
+        context.system.receptionist ! Receptionist.Subscribe(Drone.DroneServiceKey, msgAdapterListingResponse)
+//        context.system.receptionist ! Receptionist.Subscribe(BaseManager.BSManagerServiceKey, msgAdapterListingResponse)
 
-        Behaviors.receiveMessagePartial[Command] {
-          case ListingResponse(Drone.DroneServiceKey.Listing(listings)) =>
-            listings.foreach(d => context.log.info("Drone Path received from Receptionist Listing: {}", d.path))
+        Behaviors.receiveMessage[Command] {
+          case DroneReady =>
+            context.system.receptionist ! Receptionist.Find(BaseManager.BSManagerServiceKey, msgAdapterListingResponse)
             Behaviors.same
-          case ListingResponse(BaseManager.BSManagerServiceKey.Listing(listings)) =>
-            listings.foreach(bm => context.log.info("BaseManager Path received from Receptionist Listing: {}", bm.path))
+
+          case receptionistListing: ListingResponse =>
+            receptionistListing.listing match {
+              case Drone.DroneServiceKey.Listing(listings) =>
+                context.log.debug("----- Drone.DroneServiceKey.Listing received -----")
+              case BaseManager.BSManagerServiceKey.Listing(listings) =>
+                context.log.debug("----- Drone.BSManagerServiceKey.Listing received -----")
+                listings.foreach(bm => context.log.debug("received BaseManager ActorRef from Receptionist: {}", bm.hashCode()))
+                if(managerNotReady) {
+                  drone ! Drone.ManagerReady
+                  managerNotReady = false
+                }
+            }
+            Behaviors.same
+
+          case RequestBaseStation(reqId, drone) => // TODO: Restructure to ensure that Listing Set is always updated before requesting a BaseStation
+            context.log.debug(s"RequestBaseStation: reqId{$reqId}")
+            router ! BaseManager.WrappedDroneManagerMsg(RequestBaseStation(reqId, drone))
+            context.log.debug(" --- post router ! WrappedDroneManagerMsg()")
             Behaviors.same
         }
     }
   }
+}
+
+class DroneManager(context: ActorContext[DroneManager.Command]) extends AbstractBehavior[DroneManager.Command](context) {
+  override def onMessage(msg: DroneManager.Command): Behavior[DroneManager.Command] = ???
 }
