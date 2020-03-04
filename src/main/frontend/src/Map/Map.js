3becaed42933e3765e636aeec82e8a4681e9a299
Adam <omahonyadam@gmail.com>
2020-03-04 13:40:27 +0000

    add react frontend and update gitignore to ignore node_modules folder

---
+++ src/main/frontend/src/Map/Map.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from "react";
+import L, { marker } from "leaflet";
+
+  const style = {
+    width: "100%",
+    height: "300px"
+  };
+
+  const mapbox_url =
+   "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3F1cnJsaSIsImEiOiJjamhxY2lkeTcxOG9jMzdudTZkYm9jbmlkIn0.zvZPOE4XHex2E18F0FSdSg"
+
+  const map_bounds = [
+    [51.360007, -10.743767],
+    [55.391440, -5.497318]
+  ]
+
+function Map({ markerPosition }) {
+  // create map
+  const mapRef = useRef(null);
+  const [markers, setMarkers] = useState( [] );
+
+  useEffect(() => {
+    const map = L.map("map", {
+      center: [53, -7],
+      zoom: 7,
+      minZoom: 7,
+      bounds: map_bounds,
+      maxBounds: map_bounds,
+      layers: [
+        L.tileLayer(mapbox_url, {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+      ]
+    });
+
+    map.addEventListener('click', ev => {
+      console.log(`lat: ${ev.latlng.lat}, lng: ${ev.latlng.lng}`);
+    });
+
+
+    mapRef.current = map;
+  }, []);
+
+  // add marker
+  const markerRef = useRef(null);
+  useEffect(() => {
+    console.log("map stop position: " +  markerPosition)
+    markerRef.current = L.marker(markerPosition).addTo(mapRef.current);
+    let new_state = markers.push(mapRef);
+    // setMarkers(new_state)
+
+    // if(markers.length > 2) {
+      // draw line between currentRef and previous Ref
+      // L.polyline([markerRef, markers[markers.length-1]]).addTo(mapRef)
+    // }
+  
+  }, [markerPosition]);
+
+  return <div id="map" style={style} />;
+}
+
+export default Map;
---
+++ src/main/scala/ie/ul/dronenet/actors/IOSocket.scala
@@ -0,0 +1,5 @@
+package ie.ul.dronenet.actors
+
+class IOSocket {
+
+}
