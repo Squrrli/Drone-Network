c333b7687b5622e0a81fdb8b9163d1d5aeb2df4f
Adam <omahonyadam@gmail.com>
2020-03-04 13:50:10 +0000

    convert from object to class. more easily allows declaring membar values

--- src/main/scala/ie/ul/dronenet/actors/IOSocket.scala
+++ src/main/scala/ie/ul/dronenet/actors/IOSocket.scala
@@ -1,9 +1,17 @@
 package ie.ul.dronenet.actors
 
+import akka.actor.{ActorSystem, typed}
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
+sealed trait Command extends CborSerializable
+
 /**
  * Class to handle communication between frontend and Drone Network
  * Singleton actor through with all socket communication takes place.
  */
-class IOSocket {
+class IOSocket(context: ActorContext[Command]) extends AbstractBehavior[Command](context) {
+  implicit val system: typed.ActorSystem[Nothing] = context.system
 
+  override def onMessage(msg: Command): Behavior[Command] = ???
 }
