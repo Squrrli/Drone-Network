e48da2903e089003481253bc8b13bcbfd1e5da7c
Adam <omahonyadam@gmail.com>
2020-03-04 13:41:03 +0000

    initial IOSocket actor commit

--- src/main/scala/ie/ul/dronenet/actors/IOSocket.scala
+++ src/main/scala/ie/ul/dronenet/actors/IOSocket.scala
@@ -1,5 +1,9 @@
 package ie.ul.dronenet.actors
 
+/**
+ * Class to handle communication between frontend and Drone Network
+ * Singleton actor through with all socket communication takes place.
+ */
 class IOSocket {
 
 }
